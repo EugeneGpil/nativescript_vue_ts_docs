dc4a210e1ba0b537047f1a86102874d60ccd2e87
EugeneGpil <gpilligrim@gmail.com>
2022-02-05 20:51:51 +0700

    details page

--- app/components/Details.vue
+++ app/components/Details.vue
@@ -1,11 +1,36 @@
 <template>
   <Page>
-
+    <ActionBar :title="flick.title"/>
+    <ScrollView>
+      <StackLayout class="details">
+        <image :src="flick.image"/>
+        <StackLayout v-for="detail in flick.details" class="details__info" :key="detail.title">
+          <label :text="detail.title" class="details__text-primary text-primary"/>
+          <label :text="detail.body" class="details__text-secondary text-secondary"/>
+        </StackLayout>
+      </StackLayout>
+    </ScrollView>
   </Page>
 </template>
 
 <script lang="ts">
 import Vue from 'nativescript-vue'
+import FlickService from "~/services/FlickService/FlickService";
 
-export default Vue.extend({})
+const flickService = new FlickService()
+
+export default Vue.extend({
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      flick: flickService.getFlickById(this.id)
+    }
+  },
+})
 </script>
